572dd842b141b2ca78f627c847022576d8b46bea
Funmi7 <talabifunmilayo@gmail.com>
2019-10-01 16:03:54 +0100

    Refractored some parts of the code and added some basic stylings

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,13 +5,15 @@ import GithubCard from './components/GithubCard'
 import GithubFollowers from './components/GithubFollowers';
 
 const githubAPI = 'https://api.github.com/users/funmi7'
+const githubFollowersAPI = 'https://api.github.com/users/tetondan/followers'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      githubData : {}
+      githubData : {},
+      githubFollowers : []
     }
   }
 
@@ -24,23 +26,28 @@ class App extends React.Component {
     .catch(error => {
       console.log('Error: ', error)
     })
+
+    axios.get(githubFollowersAPI)
+    .then(response => {
+      this.setState({githubFollowers: response.data})
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log('Error', error)
+    })
   }
 
   render() {
    const{avatar_url, name, login, location, html_url, followers, following, bio} = this.state.githubData;
     return (
       <div className="App">
-        <GithubCard 
-        avatar_url={avatar_url}
-        name={name}
-        login={login}
-        location={location}
-        html_url={html_url}
-        followers={followers}
-        following={following}
-        bio={bio}
+        <h2>Github Users</h2>
+        <GithubCard
+         avatar_url={avatar_url}
+         login={login}
+         html_url={html_url}
         />
-        <GithubFollowers />
+        <GithubFollowers githubFollowers={this.state.githubFollowers}/>
       </div>
     );
   }
--- github-user-card/src/components/GithubFollowers.js
+++ github-user-card/src/components/GithubFollowers.js
@@ -2,40 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import GithubCard from './GithubCard';
 
-const githubFollowersAPI = 'https://api.github.com/users/tetondan/followers'
 
 export default class GithubFollowers extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            githubFollowers : []
-        }
-    }
-
-    componentDidMount() {
-        axios.get(githubFollowersAPI)
-        .then(response => {
-          this.setState({githubFollowers: response.data})
-          console.log(response.data)
-        })
-        .catch(error => {
-          console.log('Error', error)
-        })
-      }
 
       render () {
+          const {githubFollowers} = this.props;
           return (
-              this.state.githubFollowers.map(follower => {
+              githubFollowers.map(follower => {
                   return (
-                      <GithubCard
+                      <GithubCard key={follower.id}
                         avatar_url={follower.avatar_url}
-                        name={follower.name}
                         login={follower.login}
                         location={follower.location}
                         html_url={follower.html_url}
-                        followers={follower.followers}
-                        following={follower.following}
-                        bio={follower.bio}
                     />
                   )
               })
